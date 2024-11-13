29a833abe3437a708852a8dfefc7d89dc0e34384
Alxpy <achurata3@umsa.bo>
2024-11-13 00:44:54 -0400

    changes

--- src/core/services/categoria_service.py
+++ src/core/services/categoria_service.py
@@ -3,22 +3,22 @@ from src.core.abstractions.services.categoria_service_abstract import ICategoria
 from src.core.models.categoria_domain import CategoriaDomain
 
 
-class ProductoService(ICategoriaService):
+class CategoriaService(ICategoriaService):
 
     def __init__(self, categoria_repository: ICategoriaRepository):
         self.categoria_repository = categoria_repository
 
-    async def get_producto(self, idCategoria: int) -> CategoriaDomain:
+    async def get_categoria(self, idCategoria: int) -> CategoriaDomain:
         return await self.categoria_repository.get(idCategoria)
 
-    async def create_producto(self, categoria: CategoriaDomain) -> CategoriaDomain:
+    async def create_categoria(self, categoria: CategoriaDomain) -> CategoriaDomain:
         return await self.categoria_repository.create(categoria)
 
-    async def update_producto(self, idCategoria: int, categoria: CategoriaDomain) -> CategoriaDomain:
+    async def update_categoria(self, idCategoria: int, categoria: CategoriaDomain) -> CategoriaDomain:
         return await self.categoria_repository.update(idCategoria, categoria)
 
-    async def delete_producto(self, idCategoria: int) -> None:
+    async def delete_categoria(self, idCategoria: int) -> None:
         return await self.categoria_repository.delete(idCategoria)
 
-    async def get_all_productos(self) -> list[CategoriaDomain]:
+    async def get_all_categoria(self) -> list[CategoriaDomain]:
         return await self.categoria_repository.get_all()
